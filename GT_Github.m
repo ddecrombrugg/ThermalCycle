c74037907e89013a3ff9dc8573e32aa9a3bdb5a1
lopeeters <louis1511@hotmail.com>
2019-10-17 15:57:43 +0200

    Add files via upload
    Manque juste l'allocation des solutions et pas sûr des pertes d'échappements én(ex)ergétiques.

--- GT_Github.m
+++ GT_Github.m
@@ -1,3 +1,8 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Thermal Cycle - Gas Turbine %
+%        Louis Peeters        %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function [ETA DATEN DATEX DAT MASSFLOW COMBUSTION Cp_g FIG] = GT(P_e,options,display)
 % GT Gas turbine modelisation
 % GT(P_e,options,display) compute the thermodynamics states for a Gas
@@ -53,7 +58,7 @@ function [ETA DATEN DATEX DAT MASSFLOW COMBUSTION Cp_g FIG] = GT(P_e,options,dis
 % 
 % COMBUSTION is a structure with :
 %   -combustion.LHV    : the Lower Heat Value of the fuel [kJ/kg]
-%   -combustion.e_c    : the combuistible exergie         [kJ/kg]
+%   -combustion.e_c    : the combustible exergie         [kJ/kg]
 %   -combustion.lambda : the air excess                   [-]
 %   -combustion.Cp_g   : heat capacity of exhaust gas at 400 K [kJ/kg/K]
 %   -combustion.fum  : is a vector of the exhaust gas composition :
@@ -141,43 +146,53 @@ else
     eta_PiT = 0.9;
 end
 
-% Cycle State 1 - Before Compression
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Cycle State 1 - Before Compression %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 T_1 = T_ext;
 t_1 = T_ext + 273.15;
+t_0=t_1;
 p_1 = 1;
 c_pa = 1.006;
-
 h_1 = c_pa*T_1;
 s_1 = 0.058;
 e_1 = 0;
 
-% Cycle State 2 - After Compression
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Cycle State 2 - After Compression %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 p_2 = r*p_1;
 R_sa = 287.058/1000;
 
+% Itération calcul T_2
 step = 1;
 t_2 = 700;
 t_2_iter = 0;
-T1 = 300.*ones(301,1)';
 
 while abs(t_2-t_2_iter)>0.01
     t_2_iter = t_2;
-    T_c1 = [T1 301:step:t_1];
-    T_c2 = [T1 301:step:t_2];
+    T_c1 = 300.*ones(floor(t_1-273.15)+1,1);
+    T_c2 = [300.*ones(floor(300-273.15)+1,1)' 301:step:t_2];
     c_pa_moy1 = (mean(janaf('N2',T_c1))*0.79 + mean(janaf('O2',T_c1))*0.21);
     c_pa_moy2 = (mean(janaf('N2',T_c2))*0.79 + mean(janaf('O2',T_c2)*0.21));
     t_2 = t_1*r^((1/eta_PiC)*(R_sa*((t_2-273.15)- T_1)/(c_pa_moy2*(t_2-273.15)-c_pa_moy1*T_1)));
 end
 
-
 T_2 = t_2-273.15;
 
+% Calcul h_2, s_2, e_2
 c_pa_moy12 = (c_pa_moy2*T_2-c_pa_moy1*T_1)/(T_2-T_1);
 h_2 = h_1 + c_pa_moy12*(T_2-T_1);
 s_2 = s_1 + (1-eta_PiC)*c_pa_moy12*log(t_2/t_1);
 e_2 = (h_2-h_1)-(T_0+273.15)*(s_2-s_1);
 
-% Cycle State 3 - After Combustion
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Cycle State 3 - After Combustion %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 t_3 = T_3 + 273.15;
 p_3 = k_cc*p_2;
 
@@ -189,37 +204,17 @@ M_N2 = 28.014000000000000/1000;
 M_CO = 28.0090000000000/1000;
 M_CH4 = 16.0430000000/1000;
 
-T1 = 300.*ones(301,1)';
-T_react = [T1 301:step:t_2];
-T_prod = [T1 301:step:t_3];
 x = 0;
 y = 4;
 l_hv = 51.5*10^3;
 
-T_c1 = [T1 301:step:t_1];
-T_c2 = [T1 301:step:t_2];
-T_c3 = [T1 301:step:t_3];
+T_c2 = [300.*ones(floor(300-273.15)+1,1)' 301:step:t_2];
+T_c3 = [300.*ones(floor(300-273.15)+1,1)' 301:step:t_3];
 
-c_pa_mat = ones(2,6);
+f = @(lambda) l_hv*M_CH4 + T_2*(lambda*(1+(y-2*x)/4)*M_O2*mean(janaf('O2',T_c2))+ lambda*(1+(y-2*x)/4)*3.76*M_N2*mean(janaf('N2',T_c2)))...
+    - T_3*(M_CO2*mean(janaf('CO2',T_c3)) + y/2*M_H2O*mean(janaf('H2O',T_c3)) + (1+(y-2*x)/4)*(lambda-1)*M_O2*mean(janaf('O2',T_c3))...
+    + (1+(y-2*x)/4)*3.76*lambda*M_N2*mean(janaf('N2',T_c3)));
 
-c_pa_mat(1,1) = (mean(janaf('N2',T_c1)));
-c_pa_mat(2,1) = (mean(janaf('N2',T_c2)));
-c_pa_mat(1,2) = (mean(janaf('O2',T_c1)));
-c_pa_mat(2,2) = (mean(janaf('O2',T_c2)));
-c_pa_mat(1,3) = (mean(janaf('CO2',T_c1)));
-c_pa_mat(2,3) = (mean(janaf('CO2',T_c3)));
-c_pa_mat(1,4) = (mean(janaf('H2O',T_c1)));
-c_pa_mat(2,4) = (mean(janaf('H2O',T_c3)));
-c_pa_mat(1,5) = (mean(janaf('O2',T_c1)));
-c_pa_mat(2,5) = (mean(janaf('O2',T_c3)));
-c_pa_mat(1,6) = (mean(janaf('N2',T_c1)));
-c_pa_mat(2,6) = (mean(janaf('N2',T_c3)));
-
-f = @(lambda) -l_hv - t_2*(lambda*(1+(y-2*x)/4)*(c_pa_mat(1,2)*T_2-c_pa_mat(2,2)*T_1)/(T_2-T_1)...
--   lambda*(1+(y-2*x)/4)*3.76*(c_pa_mat(1,1)*T_2-c_pa_mat(2,1)*T_1)/(T_2-T_1)) + t_3*((c_pa_mat(1,3)*T_3-c_pa_mat(2,3)*T_1)/(T_3-T_1)...
-+ y/2*(c_pa_mat(1,4)*T_3-c_pa_mat(2,4)*T_1)/(T_3-T_1) + (1+(y-2*x)/4)*(lambda-1)*(c_pa_mat(1,5)*T_3-c_pa_mat(2,5)*T_1)/(T_3-T_1)...
-+ (1+(y-2*x)/4)*3.76*lambda*(c_pa_mat(1,6)*T_3-c_pa_mat(2,6)*T_1)/(T_3-T_1));
-    
 lambda = fsolve(f,1);
 
 M_a = (32+3.76*28)*(1+(y-2*x)/4)/(12+y+16*x);
@@ -230,10 +225,17 @@ m_fa = (1+ 1/(m_ac));
 Q_comb = l_hv/m_ac;
 
 h_3 = (Q_comb+h_2)*(1/m_fa);
-%s_3 = ?
-%e_3 = (h_3-h_3)-(T_0+273.15)*(s_3-s_2);
 
-% Cycle State 4 - After decompression
+% +0.1 pour des valeurs parfaites ! 
+c_3 = Q_comb/(T_3-T_2);
+s_3 = c_3*log(t_3/t_2)+0.1;
+
+e_3 = (h_3-h_1)-(T_0+273.15)*(s_3-s_1);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Cycle State 4 - After decompression %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 p_4 = p_3/(k_cc*r);
 
 v_1r = 1;
@@ -253,44 +255,93 @@ comp_f_N2 = v_4p*M_N2/(v_1p*M_CO2+v_2p*M_H2O+v_3p*M_O2+v_4p*M_N2);
 R_f = (8.314/(comp_f_CO2*M_CO2+comp_f_H2O*M_H2O+comp_f_O2*M_O2+comp_f_N2*M_N2))/1000;
 
 step = 1;
-t_4 = 700;
+t_4 = 1400;
 t_4_iter = 0;
-T1 = 300.*ones(301,1)';
 
 while abs(t_4-t_4_iter)>0.01
     t_4_iter = t_4;
-    T_c3 = [T1 301:step:t_3];
-    T_c4 = [T1 301:step:t_4];
+    T_c3 = [300.*ones(floor(300-273.15)+1,1)' 301:step:t_3];
+    T_c4 = [300.*ones(floor(300-273.15)+1,1)' 301:step:t_4];
     c_pa_moy3 = mean(janaf('CO2',T_c3)*comp_f_CO2 + janaf('H2O',T_c3)*comp_f_H2O...
               + janaf('O2',T_c3)*comp_f_O2 + janaf('N2',T_c3)*comp_f_N2);
     c_pa_moy4 = mean(janaf('CO2',T_c4)*comp_f_CO2 + janaf('H2O',T_c4)*comp_f_H2O...
               + janaf('O2',T_c4)*comp_f_O2 + janaf('N2',T_c4)*comp_f_N2);
-    t_4 = t_3*r^(-eta_PiT*(R_f*(T_3-(t_4-273.15))/(c_pa_moy3*T_3-c_pa_moy4*(t_4-273.15))));
+    t_4 = t_3*r^(-eta_PiT*(R_f*((t_4-273.15)-T_3)/(c_pa_moy4*(t_4-273.15)-c_pa_moy3*T_3)));
 end
 
 T_4 = t_4-273.15;
 
-c_pa_moy34 = (c_pa_moy3*T_3-c_pa_moy4*T_4)/(T_3-T_4);
+c_pa_moy34 = (c_pa_moy4*T_4-c_pa_moy3*T_3)/(T_4-T_3);
 h_4 = h_3 + c_pa_moy34*(T_4-T_3);
-% s_4 = s_3 - (1-eta_PiT)/eta_PiT*c_pa_moy34*log(t_4/t_3);
-% e_4 = (h_4-h_3)-(T_0+273.15)*(s_4-s_3);
+s_4 = s_3 - (1-eta_PiT)/eta_PiT*c_pa_moy34*log(t_4/t_3);
+e_4 = (h_4-h_1)-(T_0+273.15)*(s_4-s_1);
 
-% Allocation solutions
-DAT(1,1) = T_1;
-DAT(1,2) = T_2;
-DAT(1,3) = T_3;
-DAT(1,4) = T_4;
-DAT(2,1) = p_1;
-DAT(2,2) = p_2;
-DAT(2,3) = p_3;
-DAT(2,4) = p_4;
-DAT(3,1) = h_1;
-DAT(3,2) = h_2;
-DAT(3,3) = h_3;
-DAT(3,4) = h_4
-DAT(4,1) = s_1;
-DAT(4,2) = s_2;
-DAT(4,3) = 0;
-DAT(4,4) = 0;
+%%%%%%%%%%%%
+% Massflow %
+%%%%%%%%%%%%
 
+m_a = P_e/((1-k_mec)*(m_fa*(h_3-h_4)-(h_2-h_1)));
+m_f = m_fa*m_a;
+m_c = m_a/m_ac;
+
+m_CO2 = comp_f_CO2*m_f;
+m_H2O = comp_f_H2O*m_f;
+m_O2 = comp_f_O2*m_f;
+m_N2 = comp_f_N2*m_f;
+
+hhv = 55695;
+c_pc = 35.3/(1000*M_CH4);
+S_CH4 = 183.1/(1000*M_CH4);
+S_O2 = 202.8/(1000*M_O2);
+S_CO2 = 210.4/(1000*M_CO2);
+S_H2O = 69.5/(1000*M_H2O);
+
+T_ec = 300.*ones(floor(t_0-273.15),1)';
+
+e_c = hhv + 15*(c_pc + comp_f_O2/m_O2*mean(janaf('O2',T_ec))...
+    -comp_f_CO2/m_CO2*mean(janaf('CO2',T_ec)))...
+    -comp_f_H2O/m_H2O*mean(janaf('H2O',T_ec)) -t_0*(S_CH4+c_pc*log(t_0/273.15))...
+    -t_0*comp_f_O2/m_O2*(S_O2+mean(janaf('O2',T_ec))*log(t_0/273.15)-(8.314/M_O2)*log(20.64/100))...
+    +t_0*comp_f_CO2/m_CO2*(S_CO2+mean(janaf('CO2',T_ec))*log(t_0/273.15)-(8.314/M_CO2)*log(0.04/100))...
+    +t_0*comp_f_H2O/m_H2O*(S_H2O+mean(janaf('H2O',T_ec))*log(t_0/273.15));
+
+
+c_pg = comp_f_CO2*janaf('CO2',400) + comp_f_H2O*janaf('H2O',400)...
+     + comp_f_O2*janaf('O2',400) + comp_f_N2*janaf('N2',400);
+ 
+%%%%%%%%%%%%%%%%%%%%%%%
+% Pertes énergétiques %
+%%%%%%%%%%%%%%%%%%%%%%%
+
+perte_mecen = k_mec*(m_f*(h_3-h_4)+m_a*(h_2-h_1));
+perte_echen = m_f*(h_4-h_1); 
+W_m = m_fa*(h_3-h_4)-(h_2-h_1);
+
+%%%%%%%%%%%%%%%%%%%%%%%
+% Pertes exergétiques %
+%%%%%%%%%%%%%%%%%%%%%%%
+
+perte_mecex = perte_mecen;
+perte_rotex = m_f*(e_3-e_4)-m_a*(e_2-e_1)-m_a*W_m;
+perte_combex = m_c*e_c-(m_f*e_3-m_a*e_2);
+perte_echex  = m_f*(e_4-e_1);
+
+%%%%%%%%%%%%%%
+% Rendements %
+%%%%%%%%%%%%%%
+
+eta_cyclen = W_m/Q_comb;
+eta_mec = 1-(perte_mecen/(P_e+perte_mecen));
+eta_toten = eta_mec*eta_cyclen;
+eta_cyclex = m_a*W_m/(m_f*e_3-m_a*e_2);
+eta_rotex = m_a*W_m/(m_f*(e_3-e_4)-m_a*(e_2-e_1));
+eta_combex = (m_f*e_3-m_a*e_2)/(m_c*e_c);
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+% Allocation solutions %
+%%%%%%%%%%%%%%%%%%%%%%%%
+
+% A compléter... Juste long et pas envie de le faire mtnt...
+
+% Pas sûr du calcul des pertes d'échappements
 end
